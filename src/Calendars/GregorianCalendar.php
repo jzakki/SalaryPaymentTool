a7e8c04fb8f20b153cab9163202a8cd337c44728
javad.z <javad.z@bugloos.com>
2024-12-21 21:40:43 +0330

    Remove Type Hints

--- src/Calendars/GregorianCalendar.php
+++ src/Calendars/GregorianCalendar.php
@@ -18,12 +18,12 @@ class GregorianCalendar implements CalendarInterface
         return in_array($date->format('N'), [6, 7]);
     }
 
-    public function modifyDate(\DateTimeInterface $date, string $modification): \DateTimeInterface
+    public function modifyDate(\DateTimeInterface $date, string $modification)
     {
         return new \DateTime($date->format('Y-m-d') . ' ' . $modification, $this->timezone);
     }
 
-    public function modifyDate_Temp(\DateTimeInterface $date, string $modification): bool
+    public function modifyDate_Temp(\DateTimeInterface $date, string $modification)
     {
         // To do
         return 0;
