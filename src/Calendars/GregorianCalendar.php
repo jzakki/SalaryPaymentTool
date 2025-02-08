74902df30dc0eb68ae479afc7f9f17658eda4e81
javad.z <javad.z@bugloos.com>
2025-02-08 12:21:19 +0330

    Test 03

--- src/Calendars/GregorianCalendar.php
+++ src/Calendars/GregorianCalendar.php
@@ -10,7 +10,7 @@ class GregorianCalendar implements CalendarInterface
 
     public function getLastDayOfMonth(\DateTimeInterface $date)
     {
-        return new \DateTime($date->format('Y-m-t'), $this->timezone);
+        return new \DateTime($date->format('Y-m'), $this->timezone);
     }
 
     public function isWeekend(\DateTimeInterface $date)
